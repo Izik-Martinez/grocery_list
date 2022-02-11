bd366b1a2624c0103351a8da99359c8a5d7f57ae
Izik-Martinez <izikma17@yahoo.com>
2022-02-11 09:50:20 -0500

    Added functionality to delete allItems (grocery list) when clicking on pen icon

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const groceries = document.getElementsByClassName("groceries")[0];
 const pen = document.getElementsById("pen");
 const allItems = document.getElementsById("allItems");
 const userInput = getElementsById("userInput");
+
+pen.addEventListener("click", function(){
+
+ allItems.innerHTML = "";
+
+})
