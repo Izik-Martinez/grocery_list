0a202812096dc7b733feb5a76097ba219013d6fb
Izik-Martinez <izikma17@yahoo.com>
2022-02-11 09:42:53 -0500

    created access to elements within HTML file in JS

---
+++ script.js
@@ -0,0 +1,4 @@
+const groceries = document.getElementsByClassName("groceries")[0];
+const pen = document.getElementsById("pen");
+const allItems = document.getElementsById("allItems");
+const userInput = getElementsById("userInput");
